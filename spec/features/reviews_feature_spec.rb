c343bd512b6a284e8bfb952c5fd060117cfea919
alaanzr <alaanzr@gmail.com>
2015-11-25 22:39:23 +0000

    First commit

---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'reviewing' do
+  before { Restaurant.create name: 'KFC' }
+
+  scenario 'allows users to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('so so')
+  end
+
+  scenario 'it deletes a review when the parent restaurant is destroyed' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "This is amazing"
+    select '5', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'Delete KFC'
+    expect(page).not_to have_content('This is amazing')
+  end
+
+
+end
