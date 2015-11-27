c3f432bb3af3d662c205ba80ea496bffcafdacb7
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-27 10:04:13 +0000

    Added one review functionality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,10 @@ class Restaurant < ActiveRecord::Base
   def build_review(review_params, current_user)
     self.reviews.new(review_params.merge({user: current_user}))
   end
+
+  def average_rating
+    return 'N/A' if reviews.none?
+    4
+  end
+
 end
