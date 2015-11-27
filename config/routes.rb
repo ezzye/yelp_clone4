d3902c1eeb0092fedd3db379c2d51489fdb5fcb6
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-27 11:35:18 +0000

    added feature route for endorsement

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get 'restaurants' => 'restaurants#index'
-  resources :restaurants do
+  resources :restaurants, shallow: true do
     resources :reviews
+    resources :endorsements
   end
 
   # get 'test' => 'test#index'
