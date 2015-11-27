608f66ea72e07061dfd0a3d0cac5610450fb15dc
alaanzr <alaanzr@gmail.com>
2015-11-27 10:19:01 +0000

    Average rating - Multiple reviews accounted for

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,7 +10,10 @@ class Restaurant < ActiveRecord::Base
 
   def average_rating
     return 'N/A' if reviews.none?
-    4
+    p 'IDENTIFIER'
+    p reviews.count
+    p reviews.length
+    reviews.inject(0)  {|memo, review| memo + review.rating} / reviews.length
   end
 
 end
